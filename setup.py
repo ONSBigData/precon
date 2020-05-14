5e416192ad5b5e716e037947fd5c7494287f3c96
Mitchell Edmunds <mitchell.edmunds@ons.gov.uk>
2020-05-14 14:38:38 +0100

    Update setup.py - version 0.3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='precon',
-    version='0.2.0',
+    version='0.3.0',
     description='A set of functions to calculate Prices Economics statistics.',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Mitchell Edmunds',
