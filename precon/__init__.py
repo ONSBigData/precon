5a8dc17185fe47e2f56d018384e716b21a6b2279
Mitchell Edmunds <mitchell.edmunds@ext.ons.gov.uk>
2020-05-14 12:11:22 +0100

    Added rounding.py module with round_and_adjust_weights function. The function works for both Series and DataFrame and has been tested indev of OOH pipeline.

--- precon/__init__.py
+++ precon/__init__.py
@@ -1,15 +1,17 @@
 """ """
 
-from .chaining import *
-from .aggregation import * 
-from .contributions import *
-from .re_reference import *
+
 from .adjustments import *
+from .aggregation import * 
+from .chaining import *
+from .contributions import *
 from .get_stats import *
 from .helpers import *
+from .re_reference import *
+from .rounding import *
 from .prorate import *
 from .weights import *
 
 __author__ = 'Mitchell Edmunds'
 __email__ = 'mitchell.edmunds@ext.ons.gov.uk'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
---
+++ precon/rounding.py
@@ -0,0 +1,77 @@
+"""
+Functions for special rounding methods. Includes function to round
+and adjust weights to keep the sum of weights the same.
+"""
+
+import numpy as np
+import pandas as pd
+
+def round_and_adjust_weights(weights, decimals):
+    """Rounds a set of weights ensuring the rounded values sum to
+    the same total as the unrounded weights.
+    
+    Parameters
+    ----------
+    weights : DataFrame or Series
+        The weights to be rounded.
+    decimals : int
+        Number of decimal places to round each column to.
+        
+    Returns
+    -------
+    DataFrame or Series
+        The rounded and adjusted weights.
+    """
+    # Get the rounding factor and adjustment value
+    rounding_factor = 10**decimals
+    adjustment = 0.5 / rounding_factor
+    
+    if isinstance(weights, pd.core.series.Series):
+        
+        adjustments = _get_series_adjustments(
+            weights, decimals, rounding_factor, adjustment,
+        )
+        
+    elif isinstance(weights, pd.core.frame.DataFrame):
+        
+        # Create a zeros DataFrame to fill with adjustments
+        adjustments = pd.DataFrame().reindex_like(weights).fillna(0)
+        
+        for index, row in weights.iterrows():
+            adjustments.loc[index] = _get_series_adjustments(
+                row, decimals, rounding_factor, adjustment,
+            )   
+            
+    adjusted_weights = weights + adjustments
+    return adjusted_weights.round(decimals)
+          
+
+def _get_series_adjustments(weights, dec, factor, adjustment):
+    """Return a Series of weight adjustments to make"""
+    # Errors > 0.5 between rounded and unrounded means that adjustment
+    # is needed
+    errs = (weights - weights.round(dec)).sum()
+    no_of_adjustments = int((errs.round(dec) * factor))
+    
+    # Create a zeros Series to fill with adjustments
+    adjustments = pd.Series(dtype=float).reindex_like(weights).fillna(0)
+
+    to_adjust = _get_weights_to_adjust(weights, dec, no_of_adjustments)
+    adjustments.loc[to_adjust] = adjustment * np.sign(no_of_adjustments)
+    
+    return adjustments
+    
+
+def _get_weights_to_adjust(weights, dec, no_of_adjustments):
+    """Get the difference of each value from its rounded value and pick
+    weights to round by rank depending whether adjusting down or up.
+    """
+    if np.sign(no_of_adjustments) == -1:
+        asc = True
+    else:
+        asc = False
+
+    diff_ranked = (weights - weights.round(dec)).sort_values(ascending=asc)
+
+    return diff_ranked.index[range(0, abs(no_of_adjustments))]
+
