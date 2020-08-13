59b8939317272c6ba835b2e4487fe1959c29f0e9
Mitchell Edmunds <mitchell.edmunds@ext.ons.gov.uk>
2020-08-13 10:21:16 +0100

    Moved calculation of rounding_factor and adjustment to _get_adjustments. Removed .fillna(0) as unnecessary.

--- precon/rounding.py
+++ precon/rounding.py
@@ -34,56 +34,56 @@ def round_and_adjust(
     }
     iter_method = iter_dict.get(axis)
     
-    # Get the rounding factor and adjustment value
-    rounding_factor = 10**decimals
-    adjustment = 0.5 / rounding_factor
-    
     if isinstance(obj, pd.core.series.Series):
         
-        adjustments = _get_series_adjustments(
-            obj, decimals, rounding_factor, adjustment,
-        )
+        adjustments = _get_adjustments(obj, decimals)
         
     elif isinstance(obj, pd.core.frame.DataFrame):
         
-        # Create a zeros DataFrame to fill with adjustments
-        adjustments = pd.DataFrame().reindex_like(obj).fillna(0)
+        # Create an empty DataFrame to fill with adjustments
+        adjustments = pd.DataFrame().reindex_like(obj)
         
         for index, row in iter_method(obj):
            # Create a selector based on the axis
            slice_ = axis_slice(index, axis)
            
-           adjustments.loc[slice_] = _get_series_adjustments(
-               row, decimals, rounding_factor, adjustment,
-           )
+           adjustments.loc[slice_] = _get_adjustments(row, decimals)
             
     adjusted_obj = obj + adjustments
     return adjusted_obj.round(decimals)
           
 
-def _get_series_adjustments(obj, dec, factor, adjustment):
+def _get_adjustments(obj, decimals):
     """Return a Series of adjustments to make."""
+    # Get the rounding factor and adjustment value
+    rounding_factor = 10**decimals
+    adjustment = 0.5 / rounding_factor
+    
     # Errors > 0.5 between rounded and unrounded means that adjustment
     # is needed
-    errs = obj.subtract(obj.round(dec)).sum()
-    no_of_adjustments = int(errs.round(dec) * factor)
+    errs = obj.subtract(obj.round(decimals)).sum()
+    no_of_adjustments = int(errs.round(decimals) * rounding_factor)
     
     # Create a zeros Series to fill with adjustments
-    adjustments = pd.Series(dtype=float).reindex_like(obj).fillna(0)
+    adjustments = pd.Series(dtype=float).reindex_like(obj)
 
-    to_adjust = _get_values_to_adjust(obj, dec, no_of_adjustments)
+    to_adjust = _get_values_to_adjust(obj, decimals, no_of_adjustments)
     adjustments.loc[to_adjust] = adjustment * np.sign(no_of_adjustments)
     
     return adjustments
     
 
-def _get_values_to_adjust(values, dec, no_of_adjustments):
+def _get_values_to_adjust(values, decimals, no_of_adjustments):
     """Get the difference of each value from its rounded value and pick
     values to round by rank depending whether adjusting down or up.
     """
     asc = True if np.sign(no_of_adjustments) == -1 else False
 
-    diff_ranked = (values - values.round(dec)).sort_values(ascending=asc)
+    diff_ranked = (
+        values
+        .subtract(values.round(decimals))
+        .sort_values(ascending=asc)
+    )
 
     return diff_ranked.index[range(0, abs(no_of_adjustments))]
 
