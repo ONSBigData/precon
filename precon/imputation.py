1ad99acb8ab42d259cba1b5246cc63f2aca6c796
Mitchell Edmunds <mitchell.edmunds@ext.ons.gov.uk>
2020-09-15 17:22:21 +0100

    Updated docstrings, typing and minor refactors
    Added typing to base_price_imputation function and rewrote docstring. Edited/added some comments in the function body. Changed the 'base_month' argument to 'base_period' in both base_price_imputation and get_base_prices. Brought in the use of the 'bases' variable instead of 'not_bases' in get_base_prices.

--- precon/imputation.py
+++ precon/imputation.py
@@ -1,6 +1,8 @@
 """
 A set of functions for imputation methods.
 """
+from typing import Dict, Callable
+
 import numpy as np
 import pandas as pd
 
@@ -8,21 +10,31 @@ from precon.index_methods import calculate_index
 from precon.helpers import in_year_fill
 
 def base_price_imputation(
-        prices,
-        markers,
-        impute_methods,
-        base_month=1,
-        axis=1, 
-        weights=None,
-        index_method=None,
-        adjustments=None,
-        ):
-    """Impute the prices marked non-comparable using the index excl.
-    non-comparables.
+        prices: pd.DataFrame,
+        markers: pd.DataFrame,
+        impute_methods: Dict[str, Callable],
+        base_period: int = 1,
+        axis: int = 1, 
+        weights: pd.DataFrame = None,
+        index_method: str = None,
+        adjustments: pd.DataFrame = None,
+        ) -> pd.DataFrame:
+    """
+    Selects base prices for the base period and performs base price
+    imputation
+
+    This function performs base price imputations depending on the
+    parameters given. The `markers` parameter is to pass the function
+    a DataFrame of string markers or response indicators that tell the
+    function which prices to impute. The `impute_methods` parameter is
+    used to specify which imputation function should be used for a given
+    marker. The `weights` and `index_method` govern how the index that
+    is used for imputation is calculated. The `adjustments` parameter
+    is used to optionally pass in quality adjustments.
     """   
-    base_prices = get_base_prices(prices, base_month, axis=axis, ffill=False)
-        
-    # Apply quality adjustment if adjustments arg given
+    base_prices = get_base_prices(prices, base_period, axis=axis, ffill=False)
+
+    # Apply quality adjustment if adjustments arg given.
     if adjustments is not None:
         base_prices_filled = base_prices.ffill(axis)
         to_adjust = adjustments.ne(0)
@@ -34,40 +46,42 @@ def base_price_imputation(
             axis,
         )
 
-    # Fill forward the base prices but set non-comparables and
-    # "T" markers to NA
-    # to_impute = non_comparables | (markers == 'T')
     # Get the max times to impute for any year.
-    to_impute = markers.isin(impute_methods.keys())
-    times_to_impute = get_annual_max_count(to_impute, axis^1)
-
+    all_imputes = markers.isin(impute_methods.keys())
+    times_to_impute = get_annual_max_count(all_imputes, axis^1)
+    
+    # Repeat the base price imputation for the number of imputations
+    # needed, and for each indicator-method combination
     for _ in range(times_to_impute):
-        for ind, method in impute_methods.items():
-            to_impute = markers == ind
+        for indicator, method in impute_methods.items():
+            to_impute = markers == indicator
+            
             base_prices[to_impute] = method(
                 base_prices, to_impute, prices, weights, index_method, axis,
             )
     
+    # Using in_year_fill prevents discontinued prices filling beyond
+    # the year that they are discontinued
     base_prices = in_year_fill(base_prices, axis, method='ffill')
 
-    # Shift the base prices onto Feb-Jan+1 then bfill first month
+    # Shift the base prices onto Feb-Jan+1
     return base_prices.shift(1, axis=axis)
 
 
-def get_base_prices(prices, base_month=1, axis=0, ffill=True):
+def get_base_prices(prices, base_period=1, axis=0, ffill=True):
     """Returns the prices at the base month in the same shape as prices.
-    
+
     Default behaviour is to fill forward values, but can be changed to
     return NaN where not base_month by setting ffill=False.
     """
-    not_bases = prices.axes[axis].month != base_month
-   
+    bases = prices.axes[axis].month == base_period
+
     # Fill all except base months with NA
     base_prices = prices.copy()
     if axis == 0:
-       base_prices.iloc[not_bases, :] = np.nan
+       base_prices.iloc[~bases, :] = np.nan
     elif axis == 1:
-        base_prices.iloc[:, not_bases] = np.nan
+        base_prices.iloc[:, ~bases] = np.nan
     
     if ffill:
         return base_prices.ffill(axis)
@@ -77,7 +91,7 @@ def get_base_prices(prices, base_month=1, axis=0, ffill=True):
 
 def get_quality_adjusted_prices(prices, base_prices, adjustments, axis=1):
     """Applies the quality adjustments to get new base prices."""    
-    adjustment_factor = prices.div(prices-adjustments)
+    adjustment_factor = prices.div(prices - adjustments)
 
     return base_prices * adjustment_factor.cumprod(axis)
 
@@ -165,7 +179,6 @@ def impute_using_non_comparable_index(
     )
 
     return prices.div(index_excl_nc, axis) * 100
-
 
 
 def get_annual_max_count(df, axis):
