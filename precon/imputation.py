744791373a1c72326b4a68447318441850adf22d
Mitchell Edmunds <mitchell.edmunds@ext.ons.gov.uk>
2020-09-21 10:04:30 +0100

    Changed base_price_imputation function to take a bool arg to_impute
    This changed the other arguments. The function no longer accepts a dictionary of markers and impute methods, since they now all use the same method. This is done inside the function now rather than calling refactored function impute_using_index which has been deleted. The function now sets the weights to zero where imputation is needed, provides capability for shifting the imputed values so it fits both the local collection and central collection use cases, and makes use of period_window_fill in place of in_year_fill.

--- precon/imputation.py
+++ precon/imputation.py
@@ -7,7 +7,8 @@ import numpy as np
 import pandas as pd
 
 from precon.index_methods import calculate_index
-from precon.helpers import in_year_fill
+from precon.helpers import period_window_fill
+from precon.weights import reindex_weights_to_indices
 
 ImputationCallable = Callable[
     [pd.DataFrame, pd.DataFrame, pd.DataFrame, str, int], pd.DataFrame
@@ -16,8 +17,8 @@ ImputationMethods = Union[ImputationCallable, Dict[str, ImputationCallable]]
 
 def base_price_imputation(
         prices: pd.DataFrame,
-        imputation_markers: pd.DataFrame,
-        impute_method: Optional[ImputationMethods] = None,
+        to_impute: pd.DataFrame,
+        shift: bool = True,
         base_period: int = 1,
         axis: int = 1, 
         weights: Optional[pd.DataFrame] = None,
@@ -38,11 +39,16 @@ def base_price_imputation(
     is used for imputation is calculated. The `adjustments` parameter
     is used to optionally pass in quality adjustments.
     """   
-    # Set the default imputation method if none provided.
-    if impute_method is None:
-        impute_method = impute_using_index
+    if weights is not None:
+        weights = reindex_weights_to_indices(weights, prices, axis=axis)
+        weights = weights.mask(to_impute, 0)
     
-    base_prices = get_base_prices(prices, base_period, axis=axis, ffill=False)
+    # Set the default imputation method if none provided.   
+    start_prices = get_base_prices(prices, base_period, axis=axis, ffill=False)
+    base_prices = start_prices.copy()
+    
+    if not shift:
+        base_prices = base_prices.shift(1, axis=axis)
 
     # Apply quality adjustment if adjustments arg given.
     if adjustments is not None:
@@ -57,37 +63,42 @@ def base_price_imputation(
         )
 
     # Get the max times to impute for any year.
-    if imputation_markers.dtypes.all() == np.dtype('bool'):
-        imputations = imputation_markers
-    else:
-        # Convert to bool if not already.
-        imputations = imputation_markers.notna()
-
-    times_to_impute = get_annual_max_count(imputations, axis^1)
+    times_to_impute = get_annual_max_count(to_impute, axis^1)
     
     # Repeat the base price imputation for the number of imputations
     # needed, and for each indicator-method combination.
     for _ in range(times_to_impute):
-        # If a dict of impute methods is provided, loop through and 
-        # apply the method for each response indicator.
-        if isinstance(impute_method, dict):
-            for indicator, method in impute_method.items():
-                to_impute = imputation_markers == indicator
-                base_prices[to_impute] = method(
-                    base_prices, prices, weights, index_method, axis,
-                )
-                
-        else:
-            base_prices[imputations] = impute_method(
-                    base_prices, prices, weights, index_method, axis,
-            )
-    
-    # Using in_year_fill prevents discontinued prices filling beyond
-    # the year that they are discontinued
-    base_prices = in_year_fill(base_prices, axis, method='ffill')
 
-    # Shift the base prices onto Feb-Jan+1
-    return base_prices.shift(1, axis=axis)
+        base_prices_filled = base_prices.ffill(axis)
+
+        if weights is None:
+            base_prices_filled[to_impute] = np.nan
+
+        index = calculate_index(
+            prices,
+            base_prices_filled,
+            weights=weights,
+            method=index_method,
+            axis=axis^1,
+        )
+        
+        imputed_values = prices.div(index, axis) * 100
+        base_prices = base_prices.mask(to_impute, imputed_values)
+    
+    
+    # Using period_window_fill prevents discontinued prices filling
+    # beyond the year that they are discontinued
+    base_prices = period_window_fill(
+        base_prices, periods=12, freq='MS',
+        axis=axis, method='ffill',
+    )
+    
+    if shift:
+        # Shift the base prices onto Feb-Jan+1
+        base_prices = base_prices.shift(1, axis=axis)
+    
+    # Back fill the first Jan
+    return base_prices.fillna(start_prices)
 
 
 def get_base_prices(prices, base_period=1, axis=0, ffill=True):
@@ -116,44 +127,6 @@ def get_quality_adjusted_prices(prices, base_prices, adjustments, axis=1):
     adjustment_factor = prices.div(prices - adjustments)
 
     return base_prices * adjustment_factor.cumprod(axis)
-
-
-def impute_using_index(
-        base_prices: pd.DataFrame,
-        prices: pd.DataFrame,
-        weights: pd.DataFrame,
-        method: str,
-        axis: int,
-        ) -> pd.DataFrame:
-    """
-    Imputes base prices using the index value from the previous period
-
-    Parameters
-    ==========
-    base_prices: Unfilled base prices that only contain values where
-        the base price changes
-
-    to_impute: A boolean mask for values to impute
-
-    prices: The price quotes observed
-
-    weights: The weights for each price quote
-
-    method: The index method used
-
-    axis: The axis that is a time series
-    """
-    base_prices_filled = base_prices.ffill(axis)
-
-    index = calculate_index(
-        prices,
-        base_prices_filled,
-        weights=weights,
-        method=method,
-        axis=axis^1,
-    )
-
-    return prices.div(index, axis) * 100
 
 
 def get_annual_max_count(df, axis):
