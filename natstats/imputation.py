569175e8f212dd6bc4e802793053f020d377d164
Mitchell Edmunds <mitchell.edmunds@ext.ons.gov.uk>
2020-10-05 18:34:34 +0100

    Rewrote docstring for impute_base_prices and added more comments

--- natstats/imputation.py
+++ natstats/imputation.py
@@ -1,5 +1,8 @@
 """
 A set of functions for imputation methods.
+
+# TODO: Modify the functions to work with a user defined period.
+    * Look at using pd.Grouper with freq argument
 """
 from typing import Optional
 
@@ -15,41 +18,70 @@ from natstats.weights import reindex_weights_to_indices
 def impute_base_prices(
         prices: pd.DataFrame,
         to_impute: pd.DataFrame,
+        index_method: str,
         shift_imputed_values: bool = True,
         base_period: int = 1,
         axis: int = 1, 
         weights: Optional[pd.DataFrame] = None,
         # TODO: make index_method an Enum
-        index_method: str = None,
         adjustments: Optional[pd.DataFrame] = None,
         ) -> pd.DataFrame:
     """
-    Selects base prices for the base period and performs base price
-    imputation
+    Imputes base prices where specified by 'to_impute' for given prices.
 
-    This function performs base price imputations depending on the
-    parameters given. The `markers` parameter is to pass the function
-    a DataFrame of string markers or response indicators that tell the
-    function which prices to impute. The `impute_methods` parameter is
-    used to specify which imputation function should be used for a given
-    marker. The `weights` and `index_method` govern how the index that
-    is used for imputation is calculated. The `adjustments` parameter
-    is used to optionally pass in quality adjustments.
-    """   
+    Supports:
+
+        * selecting index method for the imputation index
+        * shifting imputated values to the period ahead
+        * quality adjustment
+
+    Parameters
+    ----------
+    prices: DataFrame
+        Prices to get imputed base prices for.
+    to_impute: DataFrame
+        A boolean mask of where to impute.
+    index_method: {'jevons', 'dutot', 'carli', 'laspeyres', 'geometric_laspeyres'}, str or callable, defaults to 'jevons'
+        Method to calculate the index to impute with.
+    shift_imputed_values: bool, defaults to True
+        True if imputed values are shifted onto the following period.
+    base_period: int, defaults to 1
+        Base period to select initial base prices from.
+    axis : {0 or 'index', 1 or 'columns'}, defaults to 0
+        The axis that holds the time series values.
+    weights: DataFrame, optional
+        Weights needed for laspeyres and geometric laspeyres index
+        methods.
+    adjustments: DataFrame, optional
+        Adjustment values to apply to prices for quality adjustment. If
+        there is no adjustment for a price then the adjustment value
+        should be zero.
+
+    Returns
+    -------
+    DataFrame
+        The imputed base prices.
+    """
     axis = _handle_axis(axis)
-    
+
+    # Ensure the weights are in the same shape as the prices and
+    # exclude the prices to impute from the imputation index
+    # calculation by setting weights to zero.
     if weights is not None:
         weights = reindex_weights_to_indices(weights, prices, axis=axis)
         weights = weights.mask(to_impute, 0)
     
-    # Set the default imputation method if none provided.   
+    # Get the base prices to start with from given base period.
     start_prices = get_base_prices(prices, base_period, axis=axis, ffill=False)
     base_prices = start_prices.copy()
     
     if not shift_imputed_values:
+        # Shifting because base prices need to apply to the
+        # following base period. Shifts later on after base prices have
+        # been imputed if shift_imputed_values is True.
         base_prices = base_prices.shift(1, axis=axis)
 
-    # Apply quality adjustment if adjustments arg given.
+    # Apply quality adjustment if adjustments are given.
     if adjustments is not None:
         base_prices_filled = base_prices.ffill(axis)
         to_adjust = adjustments.ne(0)
@@ -61,11 +93,12 @@ def impute_base_prices(
             axis,
         )
 
-    # Get the max times to impute for any year.
+    # Repeat base price imputation method n times where n is the
+    # max number of imputations needed in a year. This is because the
+    # index needs the newly imputed values at each new period in order
+    # to impute correctly.
+    # TODO: Does it though? Test whether this loop can be removed
     times_to_impute = get_annual_max_count(to_impute, flip(axis))
-    
-    # Repeat the base price imputation for the number of imputations
-    # needed, and for each indicator-method combination.
     for _ in range(times_to_impute):
 
         base_prices_filled = base_prices.ffill(axis)
@@ -75,6 +108,8 @@ def impute_base_prices(
         if weights is None:
             base_prices_filled[to_impute] = np.nan
 
+        # Get imputed base prices by dividing the prices by the index
+        # excluding values to impute
         index = calculate_index(
             prices,
             base_prices_filled,
@@ -88,13 +123,16 @@ def impute_base_prices(
     
     # Groupby year prevents discontinued prices filling beyond the year
     # that they are discontinued
+    # TODO: Get this to work for user defined freq
     base_prices = (
         base_prices.groupby(lambda x: x.year, axis=axis)
         .fillna(method='ffill', axis=axis)
     )
     
     if shift_imputed_values:
-        # Shift the base prices onto Feb-Jan+1
+        # Shift the base prices one period ahead so the price in the
+        # next base period uses the previous base price for calculating
+        # the index value. Also shifts imputed base prices here.
         base_prices = base_prices.shift(1, axis=axis)
     
     # Back fill the first Jan
@@ -131,4 +169,5 @@ def get_quality_adjusted_prices(prices, base_prices, adjustments, axis=1):
 
 def get_annual_max_count(df, axis):
     """Counts values present in each year for df, returns max."""
+    # TODO: Change this to work with user defined freq
     return int(df.any(axis).replace(True, 1).resample('A').sum().max())
