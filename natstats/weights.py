7c3e0904160b1ed61f0e731f8789ee4d39f46638
Mitchell Edmunds <mitchell.edmunds@ext.ons.gov.uk>
2020-10-06 17:13:34 +0100

    Added type hinting to functions in weights moduel and updated docstrings

--- natstats/weights.py
+++ natstats/weights.py
@@ -1,15 +1,17 @@
-# -*- coding: utf-8 -*-
-
+"""Functions to manipulate index weights."""
 import pandas as pd
+from pandas._typing import Axis, FrameOrSeries, FrameOrSeriesUnion
 
 from natstats.helpers import reindex_and_fill, flip
 from natstats._validation import _handle_axis
 
 
-def get_weight_shares(weights, axis=1):
+def get_weight_shares(
+        weights: FrameOrSeries,
+        axis: Axis = 1,
+        ) -> FrameOrSeries:
     """If not weight shares already, calculates weight shares."""
     axis = _handle_axis(axis)
-    
     # TODO: test precision
     if not weights.sum(axis).round(5).eq(1).all():
         return weights.div(weights.sum(axis), axis=flip(axis))
@@ -18,10 +20,15 @@ def get_weight_shares(weights, axis=1):
         return weights
 
 
-def reindex_weights_to_indices(weights, indices, axis=0):
-    """If not already indexed like indices, reindexes weights."""
+def reindex_weights_to_indices(
+        weights: FrameOrSeriesUnion,
+        indices: pd.DataFrame,
+        axis: Axis = 0,
+        ) -> pd.DataFrame:
+    """Reshapes and reindexes weights to indices, if they do not
+    already have the same shape and share the same index frequency.
+    """
     axis = _handle_axis(axis)
-    
     # Convert to a DataFrame is weight is a Series, transpose if needed
     if isinstance(weights, pd.Series):
         weights = weights.to_frame()
