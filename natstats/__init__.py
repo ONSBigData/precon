177f58b5b9cec3010398ab6bfa4fb9fa8ff120f7
Mitchell Edmunds <mitchell.edmunds@ext.ons.gov.uk>
2020-10-07 10:29:38 +0100

    Changed module API definition using __all__ instead of from package import *

--- natstats/__init__.py
+++ natstats/__init__.py
@@ -1,20 +1,24 @@
 """__init__.py"""
 
-
-from .adjustments import *
-from .aggregation import * 
-from .chaining import *
-from .contributions import *
-from .helpers import *
-from .index_methods import *
-from .imputation import *
-from .pipelines import *
-from .re_reference import *
-from .rounding import *
-from .stat_compilers import *
-from .uprate import *
-from .weights import *
-
 __author__ = 'Mitchell Edmunds'
 __email__ = 'mitchell.edmunds@ext.ons.gov.uk'
 __version__ = '0.6.0'
+__all__ = [
+    'adjustments',
+    'aggregation',
+    'chaining',
+    'contributions',
+    'helpers',
+    'index_methods',
+    'imputation',
+    'pipelines',
+    're_reference',
+    'rounding',
+    'stat_compilers',
+    'uprate',
+    'weights',
+]
+__doc__ = """
+
+
+"""
