690052c93eaed4b6d2fac21431c04c48f7c9a302
edmunm <Mitchell.Edmunds@ext.ons.gov.uk>
2020-11-03 19:27:35 +0000

    i12: Added typing/docstrings to new agg functions

--- precon/aggregation.py
+++ precon/aggregation.py
@@ -1,9 +1,15 @@
 """
 Common aggregation functions.
 """
+from typing import Sequence, Optional, Dict, Union
+
 import numpy as np
 import pandas as pd
-from pandas._typing import Axis, FrameOrSeriesUnion
+from pandas._typing import (
+    Axis,
+    FrameOrSeriesUnion,
+    Level,
+)
 
 from precon.weights import get_weight_shares, reindex_weights_to_indices
 from precon.helpers import flip
@@ -16,8 +22,7 @@ def aggregate(
         method: str = 'mean',
         axis: Axis = 1,
         ) -> pd.Series:
-    """
-    Aggregate unchained indices with weights using the given method.
+    """Aggregate indices or price relatives with weights.
 
     Supports mean aggregation by default, where the weight shares are
     calculated from the weights, and the sum product of indices and
@@ -30,8 +35,8 @@ def aggregate(
     If the weights are not the same shape as the indices, then they
     will be reshaped by the function. This allows the user to pass in
     a DataFrame of weights, with only the time periods where the
-    weights are updated, as weights are usually fixed over different
-    periods.
+    weights are updated, as weights are usually fixed over a certain
+    number of periods.
 
     Parameters
     ----------
@@ -40,7 +45,7 @@ def aggregate(
     weights: DataFrame or Series
         The weights to be aggregated with the indices. Can be a Series
         when there is only one base period in the dataset.
-    method: {'mean', 'geomean'}, str defaults to mean
+    method: {'mean', 'geomean'}, str, defaults to 'mean'
         The aggregation method.
     axis : {0 or ‘index’, 1 or ‘columns’}, default 1
         Axis along which the function is applied:
@@ -64,7 +69,7 @@ def aggregate(
     # axis before aggregating.
     weights = reindex_weights_to_indices(weights, indices, flip(axis))
 
-    # Ensure zero or NA indices have zero weight
+    # Ensure zero or NA indices have zero weight.
     weights = weights.mask(indices.isna() | indices.eq(0), 0)
 
     weight_shares = get_weight_shares(weights, axis)
@@ -73,14 +78,38 @@ def aggregate(
 
 def aggregate_level(
         indices: pd.DataFrame,
-        weights: pd.DataFrame,
-        aggregate_on: list,
+        weights: FrameOrSeriesUnion,
+        aggregate_on: Union[Level, Sequence[Level]],
         method: str = 'mean',
         axis: Axis = 1,
         ) -> pd.DataFrame:
-    """
-    Aggregates the indices for each combination of level values
-    for the levels given by aggregate_on.
+    """Aggregates the indices or price relatives in each grouping.
+    
+    The grouping is given by the `aggregate_on` parameter which should
+    be a level label or list of level labels to pass to a pandas
+    groupby. Passing in hierarchical levels higher than the one to be
+    aggregated will mean the data in those levels are retained.
+    
+    Parameters
+    ----------
+    indices: DataFrame
+        The indices or price relatives to aggregate.
+    weights: DataFrame or Series
+        The weights to be aggregated with the indices. Can be a Series
+        when there is only one base period in the dataset.
+    aggregate_on: list of level labels
+        The level labels to groupby when aggregating.        
+    method: {'mean', 'geomean'}, str, defaults to 'mean'
+        The aggregation method.
+    axis : {0 or ‘index’, 1 or ‘columns’}, default 1
+        Axis along which the function is applied:
+            * 0 or ‘index’: apply function to each column.
+            * 1 or ‘columns’: apply function to each row.
+
+    Returns
+    -------
+    DataFrame:
+        The aggregated index.
     """  
     return indices.groupby(aggregate_on, axis).apply(
         aggregate, weights, method, axis,
@@ -90,17 +119,48 @@ def aggregate_level(
 def aggregate_up_hierarchy(
         indices: pd.DataFrame, 
         weights: pd.DataFrame,
-        class_levels: list = None,
+        class_levels: Optional[Sequence[Level]] = None,
         method: str = 'mean',
         axis: Axis = 1,
-        ) -> dict:
-    """
-    Returns the aggregate at each level of the hierarchy defined by
-    class_levels as a dict, with the level names as keys (except the
-    lowest level). 
+        ) -> Dict[Level, FrameOrSeriesUnion]:
+    """Aggregate up each level of a hierarchy.
+    
+    For each level given by the `class_levels`, calculates the
+    aggregate when grouping on that level. Returns a dict with key
+    value pairs of level: aggregated indices.
     
     Requires a MultiIndex. Default behaviour sets class_levels to the 
     MultiIndex level names.
+    
+    Parameters
+    ----------
+    indices: DataFrame
+        The indices or price relatives to aggregate. Requires a
+        MultiIndex on the chosen axis.
+    weights: DataFrame or Series
+        The weights to be aggregated with the indices. Can be a Series
+        when there is only one base period in the dataset. Requires a
+        MultiIndex on the chosen axis.
+    class_levels: list of level labels
+        The class level labels that define the hierarchy aggregate up. 
+        Default behaviour sets `class_levels` to the MultiIndex level
+        names.
+    method: {'mean', 'geomean'}, str, defaults to 'mean'
+        The aggregation method.
+    axis : {0 or ‘index’, 1 or ‘columns’}, default 1
+        Axis along which the function is applied:
+            * 0 or ‘index’: apply function to each column.
+            * 1 or ‘columns’: apply function to each row.
+
+    Returns
+    -------
+    dict of DataFrame or Series:
+        Returns a dict with key value pairs of level: aggregated
+        indices.
+        
+        There is no key for the lowest level since they aren't
+        aggregated. At the highest level, the aggregated indices will
+        be a Series.
     """
     if not class_levels:
         class_levels = indices.axes[axis].names
