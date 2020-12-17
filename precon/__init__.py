167d2c56bdf815c9740ace9fb21fe19fc66ac59d
Mitchell Edmunds <Mitchell.Edmunds@ext.ons.gov.uk>
2020-12-17 14:51:57 +0000

    Bump to 0.7.0-alpha.5

--- precon/__init__.py
+++ precon/__init__.py
@@ -44,7 +44,7 @@ from precon.weights import get_weight_shares, reindex_weights_to_indices
 
 __author__ = 'Mitchell Edmunds'
 __email__ = 'mitchell.edmunds@ext.ons.gov.uk'
-__version__ = '0.7.0-alpha.4'
+__version__ = '0.7.0-alpha.5'
 __all__ = [
     'adjustments',
     'aggregation',
